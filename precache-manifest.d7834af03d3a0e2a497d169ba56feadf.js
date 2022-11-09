93b6fdb22313d30897b6d51a85ae31a33b99343b
Aki Goto <gringring000@gmail.com>
2022-11-09 21:04:14 +0900

    Updates

---
+++ precache-manifest.d7834af03d3a0e2a497d169ba56feadf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12584ac14892e87af58",
+    "url": "/Dope-menu/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "e12584ac14892e87af58",
+    "url": "/Dope-menu/static/js/main.f33c91eb.chunk.js"
+  },
+  {
+    "revision": "eb63a92825ce6b930257",
+    "url": "/Dope-menu/static/js/runtime~main.3b831fd0.js"
+  },
+  {
+    "revision": "7c0ba17f1dece9f6aa68",
+    "url": "/Dope-menu/static/js/2.2626944e.chunk.js"
+  },
+  {
+    "revision": "09822d6d24ceb819774de381b8f37fbd",
+    "url": "/Dope-menu/index.html"
+  }
+];
